1b1e55e6fc9e75a61b0fd46f3338ccde4d4e56dc
Justin <justinkmpeter@gmail.com>
2025-08-06 18:12:00 -0500

    base page and lenis scroll

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./styles/globals.scss";
+import { ReactLenis, useLenis } from "lenis/react";
+// import "lenis/dist/lenis.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        {/* <ReactLenis root /> */}
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import styles from "./page.module.scss";
 
 export default function Home() {
-  return <div className={styles.page}></div>;
+  return (
+    <div className={styles.page}>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
 }
